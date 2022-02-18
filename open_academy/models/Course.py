82144a03113f92ddc9b2f412c27f91f51373bc7e
FreamAecant <zebesresident@gmail.com>
2022-02-18 14:59:13 -0600

    Ejercicio: Definir un módulo Course

---
+++ open_academy/models/Course.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Course(models.Model):
+    _name = 'open_academy.Course'
+    _description = 'Course description'
+
+    name = fields.Char(string='Course')
+    title = fields.Char(string='Title')
+    description = fields.Text(string='Description')
+
+    # @api.depends('value')
+    # def _value_pc(self):
+    #     for record in self:
+    #         record.value2 = float(record.value) / 100
